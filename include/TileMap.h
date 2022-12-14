713c7ebae92de3b9c95fe15f701e9ef6bbae3e2b
Eduardo Lima <eduardolimrib@gmail.com>
2022-12-14 10:12:28 -0300

    [ADD] TileMap, TODO Load

---
+++ include/TileMap.h
@@ -0,0 +1,26 @@
+#include "Component.h"
+#include "TileSet.h"
+#ifndef TILEMAP_HEADERS
+#define TILEMAP_HEADERS
+
+
+class TileMap: public Component{
+public:
+    TileMap(GameObject& associated, std::string file, TileSet* tileset);
+    void Load(std::string file);
+    void SetTileSet(TileSet* tileset);
+    int& At(int x, int y, int z = 0);
+    void Render();
+    void RenderLayer(int layer, int cameraX = 0, int cameraY = 0);
+    int GetWidth();
+    int GetHeight();
+    int GetDepth();
+private:
+    std::vector<int> tileMatrix;
+    TileSet* tileset;
+    int mapWidth;
+    int mapHeight;
+    int mapDepth;
+};
+
+#endif
---
+++ src/TileMap.cpp
@@ -0,0 +1,38 @@
+#include "TileMap.h"
+
+
+TileMap::TileMap(GameObject& associated, std::string file, TileSet* tileset) : Component(associated){
+    SetTileSet(tileset);
+    Load(file);
+} 
+
+
+void TileMap::SetTileSet(TileSet* tileset){
+    this->tileset = tileset;
+}
+
+
+void TileMap::Load(std::string file){
+
+}
+
+
+int& TileMap::At(int x, int y, int z){
+   z = z * mapHeight * mapWidth;
+   y = y * mapWidth;
+   return tileMatrix.at(x+y+z);
+}
+
+int TileMap::GetDepth(){
+    return mapDepth;
+}
+
+
+int TileMap::GetHeight(){
+    return mapHeight;
+}
+
+
+int TileMap::GetWidth(){
+    return mapWidth;
+}
